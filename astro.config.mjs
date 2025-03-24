1e0d0c7a2ce6ecb855ee15eb2308c15939fda462
Marisol Henao <henao.1213@gmail.com>
2025-03-23 22:21:33 -0500

    Add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://github.com/seandsun.github.io',
+  base: 'seandsun/portfolio-marisol-henao'
+});
